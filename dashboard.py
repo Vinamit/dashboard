5fbe48eb84957b460f7bb61cca2f5b4b952d046b
ninja <yannick.letort@gmail.com>
2023-09-15 14:57:41 +0200

    Commit Dashboard Home Credit

--- dashboard.py
+++ dashboard.py
@@ -7,10 +7,10 @@ from PIL import Image
 
 
 # Image Manipulation
-@st.cache_resource
-def load_image(img):
-  im =Image.open(os.path.join(img))
-  return im
+# @st.cache_resource
+# def load_image(img):
+#  im =Image.open(os.path.join(img))
+# return im
 
 # st.image(load_image('/home/ninja/Documents/CES-DS_AnalyseGestionGrandesMassesDonnées/Module14_ProjetPersonnel/Mémoire/Home-Credit_2.png'))
 st.write("### You are welcome,","Vous êtes les bienvenus,", "Gern geschehen,", "Sois bienvenidos" )
